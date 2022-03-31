2bcd5e77c5c7e3e022b9fba08097e7242aa47310
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-31 11:03:20 -0700

    Side Bar and Header CSS

--- my-app-frontend/src/components/Header.js
+++ my-app-frontend/src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import {NavLink} from "react-router-dom"
 
 function Header(){
 
     return(
+        <div>
+            
         <header className ="App-header">
             <div>
-                <h2>Potluck Planner</h2>
+                <div className="header-name">
+                    <h2 >Potluck Planner</h2>
+                </div>
+                <div>
+                <NavLink to="/eventsbydate" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Soonest</NavLink>
+                <NavLink to="/holidays" exact activeStyle = {{background: "white", color:"green"}} className='btn m-2' id="btn-two">Holidays</NavLink>
+                <NavLink to="/birthdays" exact activeStyle = {{background: "white", color:"green"}} className='btn m-2' id="btn-two">Birthdays</NavLink>
+                <NavLink to="/celebrations" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Celebrations</NavLink>
+                <NavLink to="/miscellaneous" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Miscellaneous</NavLink>
+                </div>
             </div>
         </header>
+        </div>
     )
 }
 
--- my-app-frontend/src/components/SideBar.js
+++ my-app-frontend/src/components/SideBar.js
@@ -10,19 +10,32 @@ function SideBar({handleHolidayClick, handleAllEventsClick, handleCelebrationsCl
         <div>
             <nav id ='sidebar'>
                 <ul className= 'list-unstyled componenets' id='homeSubmenu'>
-                    
-                    <NavLink to="/createevent" exact activeStyle = {{background: "white", color:"green"}} className='btn btn-secondary m-3'>Create New Event</NavLink>
-                    <button onClick={handleAllEventsClick}>Home</button>
-                    <button onClick={handleHolidayClick}>Holidays</button>
+                {/* <button activestyle = {{background: "white", color:"green"}} className="btn m5" id="btn" onClick={handleAllEventsClick}>Home 🏠</button> */}
+                <NavLink to="/" exact activeStyle = {{background: "white", color:"green"}} className='btn m-3' id="btn-two">Home 🏠</NavLink>
+                    <NavLink to="/createevent" exact activeStyle = {{background: "white", color:"green"}} className='btn  m-3' id="btn-two">Create New Event ➕</NavLink>
+                    <NavLink to="/shoppinglist" exact activeStyle = {{background: "white", color:"green"}} className='btn m-3' id="btn-two">Shopping List 🛒</NavLink>
+                    <NavLink to="/myevents" exact activeStyle = {{background: "white", color:"green"}} className='btn m-3' id="btn-two">My Events 📓</NavLink>
+                </ul>
+                {/* <ul className='list-unstyled components' id="homeSubmenu"> */}
+                {/* <NavLink to="/holidays" exact activeStyle = {{background: "white", color:"green"}} className='btn m-2' id="btn-two">Holidays</NavLink>
+                <NavLink to="/birthdays" exact activeStyle = {{background: "white", color:"green"}} className='btn m-2' id="btn-two">Birthdays</NavLink>
+                <NavLink to="/celebrations" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Celebrations</NavLink>
+                <NavLink to="/miscellaneous" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Miscellaneous</NavLink>
+                <NavLink to="/eventsbydate" exact activeStyle = {{background: "white", color:"green"}} className='btn m-1' id="btn-two">Sort by Soonest</NavLink> */}
+
+
+                
+                    {/* <button onClick={handleHolidayClick}>Holidays</button>
                     <button onClick={handleBirthdaysClick}>Birthdays</button>
                     <button onClick={handleCelebrationsClick}>Celebrations</button>
                     <button onClick={handleMiscellaneousClick}>Miscellaneous</button>
-                    <NavLink to="/myevents" exact activeStyle = {{background: "white", color:"green"}} className='btn btn-secondary m3'>My Events</NavLink>
-                    <NavLink to="/shoppinglist" exact activeStyle = {{background: "white", color:"green"}} className='btn btn-secondary m3'>Shopping List</NavLink>
+                
+        
                     <div>
                         <button onClick={handleFilterDateClick}>filter Feed by date</button>
-                    </div>
-            </ul>
+                    </div> */}
+            {/* </ul> */}
+
             </nav>
         </div>
     )
