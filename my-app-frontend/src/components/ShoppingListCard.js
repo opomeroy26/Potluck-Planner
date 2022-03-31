6ca6e6b73ea45493869587347e04c1c24596ffe9
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-31 14:50:41 -0700

    Shopping List css

--- my-app-frontend/src/components/ShoppingListCard.js
+++ my-app-frontend/src/components/ShoppingListCard.js
@@ -3,10 +3,17 @@ import React from 'react';
 function ShoppingListCard({list, handleShoppingListDelete}){
     // const {item} = list
     return(
-        <div className="card border-light item" style={{ width: '35rem'}}>
-            <div className="d-flex">
+        <div className='row content p-2'style={{ width: '35rem'}}>
+            <div className="card border-light">
+            <div className="content d-flex">
                      <li>{list.item}</li>
-                     <button onClick={()=>handleShoppingListDelete(list)}>delete</button>
+                     </div>
+                     <div>
+                     <button className='btn btn-secondary' id='listbtn' onClick={()=>handleShoppingListDelete(list)}>Remove Item</button>
+                     </div>
+               
+                {/* <button className='btn btn-secondary ' id='btn' onClick={()=>handleShoppingListDelete(list)}>delete</button> */}
+                     
             </div>
         </div>
        
